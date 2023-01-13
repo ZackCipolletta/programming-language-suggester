b8ef26c5d12a3bb95f1e8b91a0ca2f3b94bf9cb2
Zachary Cipolletta <ZackCipolletta@gmail.com>
2023-01-13 11:28:04 -0800

    Add input handler to question 4 and update the functions for the first 4 questions to handle branching based on the input values.

--- js/scripts.js
+++ js/scripts.js
@@ -1,27 +1,55 @@
 
 
-function qeustion1() {
-  const programmingKnowledge = document.getElementById('langKnowledge').value;
+function question1() {
+  const programmingKnowledge = parseInt(document.getElementById('langKnowledge').value);
   let knowledge = 'none';
-  if (programmingKnowledge === 77){
-    knowledge = 'something'
-  } else if (programmingKnowledge === 78){
-    knowldge = 'something'
-  } else if (programmingKnowledge === 79){
-    knowldge = 'something'
-  }
-  return programmingKnowledge;
+    if (programmingKnowledge === 1){
+      knowledge = 'something1'
+    } else if (programmingKnowledge === 2){
+      knowledge = 'something2'
+    } else if (programmingKnowledge === 3){
+      knowledge = 'something3'
+    }
+  return knowledge;
 }
 
 function question2() {
   const frontOrBack = parseInt(document.querySelector("input[name='frontOrBack']:checked").value);
-
-  return frontOrBack
+  let frontBack = 'none';
+    if (frontOrBack === 1){
+      frontBack = 'something1'
+    } else if (frontOrBack === 2){
+      frontBack = 'something2'
+    } else if (frontOrBack === 3){
+      frontBack = 'something3'
+    }
+  return frontBack;
 }
-
 
 function question3() {
   const preferredSmell = parseInt(document.querySelector("input[name='smells']:checked").value);
+  let smells= 'none';
+    if (preferredSmell === 1){
+      smells = 'smells1'
+    } else if (preferredSmell === 2){
+      smells = 'smells2'
+    } else if (preferredSmell === 3){
+      smells = 'smells3'
+    }
+  return smells;
+}
 
-  return preferredSmell
+function question4() {
+  const interests = parseInt(document.getElementById('genInterests').value);
+  let interest = 'none';
+    if (interests === 1){
+      interest = 'something'
+    } else if (interests === 2){
+      interest = 'something 2'
+    } else if (interests === 3){
+      interest = 'something 3'  
+    } else if (interests === 3){
+      interest = 'something 4'
+    }
+  return interest;
 }
