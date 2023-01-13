5cbb5435c3d182d11c3f26882c38d21919fde7ff
Zachary Cipolletta <ZackCipolletta@gmail.com>
2023-01-13 15:32:03 -0800

    Refactor code to make it easier to read, especially index.html.  Add additional information about each language in the results. Input field added to accept a user name which will also appear in the results. All functions will work correctly upon hitting submit button.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 
-function submitFunc(event) {
-  event.preventDefault();
+function submitFunc(e) {
+  e.preventDefault();
 
   question1();
   question2();
@@ -8,6 +8,8 @@ function submitFunc(event) {
   question4();
   question5();
   resultsFunc();
+  clearFunc();
+  printResults();
 }
 
 function question1() {
@@ -16,11 +18,12 @@ function question1() {
     if (programmingKnowledge === 1){
       knowledge = 1;
     } else if (programmingKnowledge === 2){
-      knowledge = 2;
+      knowledge = 2;    
     } else if (programmingKnowledge === 3){
-      knowledge = 3;
+      knowledge = 3;    
+    } else if (programmingKnowledge === 0){
+      knowledge = 0;
     }
-
   return knowledge;
 }
 
@@ -31,10 +34,9 @@ function question2() {
       frontBack = 1;
     } else if (frontOrBack === 2){
       frontBack = 2;
-    } else if (frontOrBack === 3){
-      frontBack = 3;
+    } else if (frontOrBack === 0){
+      frontBack = 0;
     }
-
   return frontBack;
 }
 
@@ -45,10 +47,9 @@ function question3() {
       smells = 1;
     } else if (preferredSmell === 2){
       smells = 2;
-    } else if (preferredSmell === 3){
-      smells = 3;
+    } else if (preferredSmell === 0){
+      smells = 0;
     }
-
   return smells;
 }
 
@@ -61,10 +62,9 @@ function question4() {
       interest = 2;
     } else if (interests === 3){
       interest = 3; 
-    } else if (interests === 4){
-      interest = 4;
+    } else if (interests === 0){
+      interest = 0;
     }
-
   return interest;
 }
 
@@ -77,21 +77,37 @@ function question5() {
       food = 2;
     } else if (preferreBreakfast === 3){
       food = 3;
-    } else if (preferreBreakfast === 4){
-      food = 4;
+    } else if (preferreBreakfast === 0){
+      food = 0;
     }
   return food;
 }
 
 function resultsFunc() {
   const formResults = question1() + question2()+ question3() + question4() + question5();
-
-
-  console.log(formResults);
-  return formResults;
+  const name = document.getElementById('user-name').value;
+  let results = '';
+    if (formResults >= 10){
+      results = "C#"+document.getElementById('c-sharp').removeAttribute('class');
+    } else if (formResults >= 6){
+      results = "JavaScript"+document.getElementById('javaScript').removeAttribute('class');
+    } else if (formResults >= 3){
+      results = "Python"+document.getElementById('python').removeAttribute('class');    
+    } else if (formResults <= 2){
+      results = "HQ9+"+document.getElementById('HQ9+').removeAttribute('class');
+    }
+    results = name.charAt(0).toUpperCase() + name.slice(1) + ', it looks like you should learn ' + results.slice(0, -9) + '.';
+  return results;
 }
 
+function clearFunc(){
+  return document.querySelectorAll('span').forEach(el=>el.classList.add('hidden'));
+}
 
+function printResults() {
+  const resultsPrint = document.querySelector(".survey-results");
+    resultsPrint.innerHTML = resultsFunc();
+}
 
 
 
@@ -99,5 +115,4 @@ function resultsFunc() {
 
 window.addEventListener("load", function(){
   document.querySelector(".button").addEventListener("click", submitFunc);
-
 });
